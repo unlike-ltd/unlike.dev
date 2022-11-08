5d798cb857c417e914d3ca3c8537040e19c2d8c2
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2022-11-08 12:02:00 +0000

    chore: meta description

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,10 @@ export default function Head() {
     <>
       <title>Unlike</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Unlike Ltd - Web Development" />
+      <meta
+        name="description"
+        content="Unlike others. Software development. React + TypeScript + GraphQL = 😍"
+      />
       {/* <link rel="icon" href="/favicon.ico" /> */}
     </>
   )
