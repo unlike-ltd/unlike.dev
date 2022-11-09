19347bf47c3e0c4337117b92df5d45455e4c5bc5
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2022-11-09 15:35:51 +0000

    feat: only load analytics in production

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { Analytics } from '@vercel/analytics/react'
 
+const PRODUCTION: boolean = process.env.NODE_ENV === 'production'
+
 export function AnalyticsWrapper() {
-  return <Analytics />
+  return PRODUCTION ? <Analytics /> : null
 }
